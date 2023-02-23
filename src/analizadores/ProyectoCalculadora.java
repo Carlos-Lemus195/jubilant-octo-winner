2ffc34f16241c1c557c2b86e860543301d762034
Carlos Lemus <202000195@udv.edu.gt>
2023-02-23 09:09:52 -0600

    initial commit

---
+++ src/analizadores/ProyectoCalculadora.java
@@ -0,0 +1,9 @@
+package analizadores;
+
+public class ProyectoCalculadora {
+
+    public static void main(String[] args) {
+        FrmPrincipal f = new FrmPrincipal();
+        f.setVisible(true);
+    }
+}
